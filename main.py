17bb22b528278e936305d804b16e4021c6d994b0
adrian-sliacky-gymzv <120519032+adrian-sliacky-gymzv@users.noreply.github.com>
2022-12-13 23:12:41 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-basic.show_leds("""
-.....
-.....
-..#..
-.....
-.....
-""")
 def on_forever():
     pass
 basic.forever(on_forever)
+x = 0
+y = 0
+def tick():
+    if input.button_is_pressed(Button.A):
+        led.plot(x, y)
+        x = x + 1
+        y = y + 1
+
+    # basic.pause(100)
+    # led.unplot(0, 0)
+loops.every_interval(500, tick)
--- main.ts
+++ main.ts
@@ -1,10 +1,17 @@
-basic.showLeds(`
-.....
-.....
-..#..
-.....
-.....
-`)
 basic.forever(function on_forever() {
+    
+})
+let x = 0
+let y = 0
+//  basic.pause(100)
+//  led.unplot(0, 0)
+loops.everyInterval(500, function tick() {
+    let x: number;
+    let y: number;
+    if (input.buttonIsPressed(Button.A)) {
+        led.plot(x, y)
+        x = x + 1
+        y = y + 1
+    }
     
 })
