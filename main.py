25f4c4282865c8c6af7221f58a7c9612d3825c72
adrian-sliacky-gymzv <120519032+adrian-sliacky-gymzv@users.noreply.github.com>
2022-12-13 23:20:52 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+# def on_forever():
+#     pass
+# basic.forever(on_forever)
 x = 0
 y = 0
 def tick():
+    global x, y
     if input.button_is_pressed(Button.A):
+        if x > 0:
+            led.unplot(x-1, y)
         led.plot(x, y)
         x = x + 1
-        y = y + 1
+    else:
+        led.unplot(x, y)
 
+    if input.button_is_pressed(Button.B):
+        if y > 0:
+            led.unplot(x, y-1)
+        led.plot(x, y)
+        y = y + 1
+    else:
+        led.unplot(x, y)
     # basic.pause(100)
-    # led.unplot(0, 0)
+
 loops.every_interval(500, tick)
--- main.ts
+++ main.ts
@@ -1,17 +1,31 @@
-basic.forever(function on_forever() {
-    
-})
+//  def on_forever():
+//      pass
+//  basic.forever(on_forever)
 let x = 0
 let y = 0
 //  basic.pause(100)
-//  led.unplot(0, 0)
 loops.everyInterval(500, function tick() {
-    let x: number;
-    let y: number;
+    
     if (input.buttonIsPressed(Button.A)) {
+        if (x > 0) {
+            led.unplot(x - 1, y)
+        }
+        
         led.plot(x, y)
         x = x + 1
+    } else {
+        led.unplot(x, y)
+    }
+    
+    if (input.buttonIsPressed(Button.B)) {
+        if (y > 0) {
+            led.unplot(x, y - 1)
+        }
+        
+        led.plot(x, y)
         y = y + 1
+    } else {
+        led.unplot(x, y)
     }
     
 })
