9fe98e9eaca80aef86829eae9c7616ad3985cdde
adrian-sliacky-gymzv <120519032+adrian-sliacky-gymzv@users.noreply.github.com>
2022-12-14 14:45:00 +0100

    refactoring

--- main.ts
+++ main.ts
@@ -1,18 +1,12 @@
 let x = 0
 let y = 0
-
-loops.everyInterval(500, function onEvery_interval() {
-
+loops.everyInterval(500, function () {
     if (input.buttonIsPressed(Button.A)) {
         x = (x + 1) % 5
     }
-
     if (input.buttonIsPressed(Button.B)) {
         y = (y + 1) % 5
     }
     basic.clearScreen()
     led.plot(x, y)
-    
-    console.log(x)
-    console.log(y)
 })
