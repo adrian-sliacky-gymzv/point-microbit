8ad8f9226d426b4ce917a4e02702c9480f10ee58
adrian-sliacky-gymzv <120519032+adrian-sliacky-gymzv@users.noreply.github.com>
2022-12-14 21:13:33 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,31 @@
 let x = 0
 let y = 0
-loops.everyInterval(500, function () {
+let delay_ms = 500
+let blink = false
+loops.everyInterval(delay_ms, function () {
     if (input.buttonIsPressed(Button.A)) {
         x = (x + 1) % 5
     }
     if (input.buttonIsPressed(Button.B)) {
         y = (y + 1) % 5
     }
-    basic.clearScreen()
-    led.plot(x, y)
+
+    if (blink) {
+        basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+        led.unplot(x, y)
+    }
+    else {
+        basic.clearScreen()
+        led.plot(x, y)
+    }
+        
+    console.log({x, y})
 })
 
-basic.forever(function() {
-    if (input.buttonIsPressed(Button.AB)) {}
-})
+if (input.buttonIsPressed(Button.AB)) {} // for initializing A+B button in makecode Simulator
